fa0e5d4880959ead44b685dc8babe80267479ae7
Lautaro Bachmann <laurigbach@gmail.com>
2023-04-05 11:41:08 -0300

    Apply refactoring to all files

--- connection.py
+++ connection.py
@@ -8,6 +8,7 @@ import os
 from constants import *
 from base64 import b64encode
 
+
 class Connection(object):
     """
     Conexión punto a punto entre el servidor y un cliente.
@@ -15,87 +16,104 @@ class Connection(object):
     que termina la conexión.
     """
 
-    def __init__(self, socket: socket.socket , directory):
+    def __init__(self, socket: socket.socket, directory):
         self.directory = directory
         self.socket = socket
         self.active = True
-        self.bof = ''
-    
-    def contact(self, msj: bytes or str, codif='ascii'):
-        #se va enviar el mensaje y se fija en codifi ya que es la codificacion que va a usar
-        if codif == 'ascii':
+        self.bof = ""
+
+    def contact(self, msj: bytes or str, codif="ascii"):
+        # se va enviar el mensaje y se fija en codifi ya que es la codificacion que va a usar
+        if codif == "ascii":
             msj = msj.encode("ascii")
-        elif codif == 'b64encode':
+        elif codif == "b64encode":
             msj = b64encode(msj)
         else:
-            raise ValueError(f"send: Invalid instance '{codif}'") # excepción de tipo ValueError cuando hay un valor invalido en codif
-        
-        while len(msj) > 0: #se encarga de enviar el mensaje a través del socket en trozos hasta que se haya enviado todo el mensaje
-            sent = self.socket.send(msj) 
+            # excepción de tipo ValueError cuando hay un valor invalido en codif
+            raise ValueError(f"send: Invalid instance '{codif}'")
+        # se encarga de enviar el mensaje a través del socket en trozos hasta que se haya enviado todo el mensaje
+        while len(msj) > 0:
+            sent = self.socket.send(msj)
             msj = msj[sent]
-        #sent tiene la cantidad almacena la cantidad de bytes que se han enviado y se asegura que se haya enviado al menos un byte antes de seguir
-    
+        # sent tiene la cantidad almacena la cantidad de bytes que se han enviado y se asegura que se haya enviado al menos un byte antes de seguir
+
     def quit(self):
         rta = mtext(CODE_OK) + EOL
         self.contact(rta)
-        self.active = False #desactivo la conexion 
+        # desactivo la conexion
+        self.active = False
         f"Closing connection..."
-    
+
     def valid_file(self, filename: str):
-        aux = set(filename) - VALID_CHARS #obtener los caracteres en filename que no pertenecen al conjunto VALID_CHARS
-        return (len(aux) == 0) #si obtengo != 0 significa que tengo argumentos invalidos y si tengo 0 esta todo bien
-    
-    
+        # obtener los caracteres en filename que no pertenecen al conjunto VALID_CHARS
+        aux = set(filename) - VALID_CHARS
+        # si obtengo != 0 significa que tengo argumentos invalidos y si tengo 0 esta todo bien
+        return len(aux) == 0
+
     def get_file_listing(self):
-        rta = mtext(CODE_OK) + EOL #Cadena de texto que indica que la operacion fue exitosa
-        
-        for fil in os.listdir(self.directory): #obtengo la lista de archivos disponibles en el directorio 
-            rta += fil + EOL # voy agregando los archvios a la cadena de respuesta con \r\n al final 
-        rta += EOL # fin de la cadena
+        # Cadena de texto que indica que la operacion fue exitosa
+        rta = mtext(CODE_OK) + EOL
+
+        # obtengo la lista de archivos disponibles en el directorio
+        for fil in os.listdir(self.directory):
+            # voy agregando los archvios a la cadena de respuesta con \r\n al final
+            rta += fil + EOL
+        # fin de la cadena
+        rta += EOL
         self.contact(rta)
-    
+
     def get_metadata(self, filename: str):
         rta = mtext(CODE_OK) + EOL
-        
-        if not(os.path.isfile(os.path.join(self.directory, filename))): #nos fijamos si no es un arhivo la os.path.join forma una ruta completa con self.directory y el archivo filename y dsp ve si es un archivo que existe
+        # nos fijamos si no es un arhivo la os.path.join forma una ruta completa con self.directory y el archivo filename y dsp ve si es un archivo que existe
+        if not (os.path.isfile(os.path.join(self.directory, filename))):
             rta += mtext(FILE_NOT_FOUND) + EOL
             self.contact(rta)
         elif not self.valid_file(filename):
             rta += mtext(INVALID_ARGUMENTS) + EOL
             self.contact(rta)
         else:
-            size = os.path.getsize(os.path.join(self.directory, filename)) # devuelve el tamaño en bytes de un archivo en el camino especificado
+            # devuelve el tamaño en bytes de un archivo en el camino especificado
+            size = os.path.getsize(os.path.join(self.directory, filename))
             rta = f"{str(size)} {EOL}"
-            self.contact(rta)               
+            self.contact(rta)
 
     def get_slice(self, filename: str, offset: int, size: int):
         filepath = os.path.join(self.directory, filename)
-        if not os.path.isfile(filepath): # existe archivo?
+        if not os.path.isfile(filepath):  # existe archivo?
             rta = mtext(FILE_NOT_FOUND) + EOL
             self.contact(rta)
-        elif not self.valid_file(filename): # argumentos validos?
+        elif not self.valid_file(filename):  # argumentos validos?
             rta = mtext(INVALID_ARGUMENTS) + EOL
             self.contact(rta)
         else:
             file_size = os.path.getsize(filepath)
-            if offset < 0 or offset >= file_size or size < 0 or offset + size > file_size:
+            if (
+                offset < 0
+                or offset >= file_size
+                or size < 0
+                or offset + size > file_size
+            ):
                 rta = mtext(BAD_OFFSET) + EOL  # error con el offset
                 self.contact(rta)
             rta = mtext(CODE_OK) + EOL
             self.contact(rta)
-            with open(filepath, "rb") as f: #Abrir el archivo en modo lectura binario "rb", 'r' se abrira el archivo en modo lectura y 'b' se abrira en modo binario
-                f.seek(offset) #lee el slice del archivo especificado, empezando desde el offset
-                slice_data = f.read(size) #y leyendo el tamaño especificado
-                self.contact(slice_data, codif='b64encode') #Codifica el slice en base64
+            # Abrir el archivo en modo lectura binario "rb", 'r' se abrira el archivo en modo lectura y 'b' se abrira en modo binario
+            with open(filepath, "rb") as f:
+                # lee el slice del archivo especificado, empezando desde el offset
+                f.seek(offset)
+                # y leyendo el tamaño especificado
+                slice_data = f.read(size)
+                # Codifica el slice en base64
+                self.contact(slice_data, codif="b64encode")
             rta = EOL
             self.contact(rta)
-    
+
     def _recv(self):
         try:
             while True:
-                data = self.socket.recv(4096).decode('ascii')
+                data = self.socket.recv(4096).decode("ascii")
                 self.bof += data
-                
+
                 if len(data) == 0:
                     self.active = False
                     break
@@ -103,8 +121,8 @@ class Connection(object):
             rta = mtext(BAD_REQUEST) + EOL
             self.send(rta)
             self.active = False
-            f"Closing connection..."    
-    
+            f"Closing connection..."
+
     def handle(self):
         """
         Atiende eventos de la conexión hasta que termina.
@@ -116,26 +134,26 @@ class Connection(object):
                 line = line.strip()
                 if len(line) > 0:
                     try:
-                        cmd, *args = line.split(' ')
-                        if cmd == 'get_file_listing':
+                        cmd, *args = line.split(" ")
+                        if cmd == "get_file_listing":
                             self.get_file_listing()
-                        elif cmd == 'get_metadata':
+                        elif cmd == "get_metadata":
                             if len(args) == 1:
                                 self.get_metadata(args[0])
                             else:
                                 rta = mtext(BAD_REQUEST) + EOL
                                 self.contact(rta)
-                        elif cmd == 'get_slice':
+                        elif cmd == "get_slice":
                             if len(args) == 3:
                                 self.get_slice(args[0], int(args[1]), int(args[2]))
                             else:
                                 rta = mtext(BAD_REQUEST) + EOL
                                 self.contact(rta)
-                        elif cmd == 'quit':
+                        elif cmd == "quit":
                             self.quit()
                         else:
                             rta = mtext(BAD_REQUEST) + EOL
-                            self.contact(rta) 
+                            self.contact(rta)
                     except Exception as e:
                         f"Error in connection handling: {e}"
                         rta = mtext(INTERNAL_ERROR) + EOL
--- server.py
+++ server.py
@@ -20,22 +20,21 @@ class Server(object):
     especificados donde se reciben nuevas conexiones de clientes.
     """
 
-    def __init__(self, addr=DEFAULT_ADDR, port=DEFAULT_PORT,
-                 directory=DEFAULT_DIR):
+    def __init__(self, addr=DEFAULT_ADDR, port=DEFAULT_PORT, directory=DEFAULT_DIR):
         # Revisar que el directorio exista, y si no, crearlo
         if not os.path.exists(directory):
-
-            try: os.makedirs(directory)
+            try:
+                os.makedirs(directory)
             except OSError:
                 print("No se pudo crear el directorio %s." % directory)
                 sys.exit(1)
 
         print("Serving %s on %s:%s." % (directory, addr, port))
-        
+
         # Se crea el socket y se lo vincula a la dirección y puerto
         oursocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        oursocket.bind((addr,port))
-        
+        oursocket.bind((addr, port))
+
         # Se guarda el socket y el directorio compartido en el objeto
         self.socket = oursocket
         self.directory = directory
@@ -48,41 +47,45 @@ class Server(object):
         self.socket.listen()
 
         while True:
-            (cnSocket, cnAdress) = self.socket.accept() # Bloquea la ejecución hasta que se recibe una conexión entrante
-            cn = connection.Connection(cnSocket, self.directory) # Crea un objeto Connection para manejar la conexión entrante
-            cn.handle() # Atiende la conexión entrante
+            # Bloquea la ejecución hasta que se recibe una conexión entrante
+            (cnSocket, cnAdress) = self.socket.accept()
+            # Crea un objeto Connection para manejar la conexión entrante
+            cn = connection.Connection(cnSocket, self.directory)
+            cn.handle()  # Atiende la conexión entrante
 
-#Esta función main() es el punto de entrada del programa que lanza un servidor que utiliza el protocolo de transferencia de archivos casero HFTP
+
+# Esta función main() es el punto de entrada del programa que lanza un servidor que utiliza el protocolo de transferencia de archivos casero HFTP
 def main():
     """Parsea los argumentos y lanza el server"""
-    #configurar la dirección IP, el número de puerto y el directorio compartido del servidor
+    # configurar la dirección IP, el número de puerto y el directorio compartido del servidor
     parser = optparse.OptionParser()
     parser.add_option(
-        "-p", "--port",
-        help="Número de puerto TCP donde escuchar", default=DEFAULT_PORT)
+        "-p", "--port", help="Número de puerto TCP donde escuchar", default=DEFAULT_PORT
+    )
     parser.add_option(
-        "-a", "--address",
-        help="Dirección donde escuchar", default=DEFAULT_ADDR)
+        "-a", "--address", help="Dirección donde escuchar", default=DEFAULT_ADDR
+    )
     parser.add_option(
-        "-d", "--datadir",
-        help="Directorio compartido", default=DEFAULT_DIR)
-    #Verifica si hay argumentos adicionales después de las opciones. 
-    #Si se proporcionan argumentos adicionales, muestra la ayuda del programa y sale del programa.
+        "-d", "--datadir", help="Directorio compartido", default=DEFAULT_DIR
+    )
+    # Verifica si hay argumentos adicionales después de las opciones.
+    # Si se proporcionan argumentos adicionales, muestra la ayuda del programa y sale del programa.
     options, args = parser.parse_args()
     if len(args) > 0:
         parser.print_help()
         sys.exit(1)
     try:
-        port = int(options.port) #Convierte el valor de puerto proporcionado en un número entero.
+        # Convierte el valor de puerto proporcionado en un número entero.
+        port = int(options.port)
     except ValueError:
-        sys.stderr.write(
-            "Numero de puerto invalido: %s\n" % repr(options.port))
+        sys.stderr.write("Numero de puerto invalido: %s\n" % repr(options.port))
         parser.print_help()
         sys.exit(1)
-    
-    server = Server(options.address, port, options.datadir) #Crea un objeto servidor utilizando la dirección IP, el número de puerto y el directorio compartido especificados.
-    server.serve() #Llama al método serve() en el objeto servidor para que comience a escuchar conexiones entrantes.
+    # Crea un objeto servidor utilizando la dirección IP, el número de puerto y el directorio compartido especificados.
+    server = Server(options.address, port, options.datadir)
+    # Llama al método serve() en el objeto servidor para que comience a escuchar conexiones entrantes.
+    server.serve()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
